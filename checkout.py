3f46563052cb7037e28438e026728b35c856897c
ms-uppal <muppal@umich.edu>
2026-09-17 13:50:43 -0400

    Update checkout.py
    Add docstrings to checkout.py

--- checkout.py
+++ checkout.py
@@ -1,5 +1,6 @@
-# checkout.py
+# checkout.py - Basic checkout tool
 def calculate_total(price, tax):
+    """Calculates the final cost including sales tax."""
     return price + (price * tax)
 
 print("Total price is:", calculate_total(100, 0.06))
