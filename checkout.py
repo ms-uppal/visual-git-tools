a827f75fe6381b59a533978e9a1759b556f69a70
ms-uppal <muppal@umich.edu>
2026-09-17 14:20:35 -0400

    Update checkout.py

--- checkout.py
+++ checkout.py
@@ -1,5 +1,6 @@
 # checkout.py
 def calculate_total(price, tax):
     total = price + (price * tax)
-    return total # Notable modification
+    #return total # Notable modification
+    return round(total) # Modified calculation
 
