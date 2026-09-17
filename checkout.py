c046fe143300685f44214eaed6c5acda0403e503
ms-uppal <muppal@umich.edu>
2026-09-17 14:41:58 -0400

    Update checkout.py
    Add discount calculation functionality

--- checkout.py
+++ checkout.py
@@ -3,4 +3,7 @@ def calculate_total(price, tax):
     total = price + (price * tax)
     #return total # Notable modification
     return round(total) # Modified calculation
+# Added calculation function for discounts
+def apply_discount(price, discount):
+    return price - discount
 
